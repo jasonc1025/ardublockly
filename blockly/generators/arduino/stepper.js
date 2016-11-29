4c32c0af12709c2cbd233efe8ab0ca3472647f09
jasonc1025 <jasonc@eande.world>
2016-11-29 01:33:56 -0800

    Revert "Feature Lcd: Part 1a ~ Separate .js files: Lcd.js"
    This reverts commit 6238076e940d2220f1924976f8a8d21589a98eaa.

--- blockly/generators/arduino/stepper.js
+++ blockly/generators/arduino/stepper.js
@@ -56,6 +56,48 @@ Blockly.Arduino['stepper_config'] = function(block) {
 
 
 /**
+ * Code generator for the stepper generator configuration. Nothing is added
+ * to the 'loop()' function. Sets the pins (X and Y), steps per revolution (Z),
+ * speed(A) and instance name (B).
+ * Arduino code: #include <Stepper.h>
+ *               Stepper B(Z, X, Y);
+ *               setup() { B.setSpeed(A); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Empty string as no code goes into 'loop()'.
+ */
+// // Blockly.Arduino['stepper_config_jwc'] = function(block) {
+Blockly.Arduino['lcd_config_jwc'] = function(block) {
+  var pin1 = block.getFieldValue('STEPPER_PIN1');
+  var pin2 = block.getFieldValue('STEPPER_PIN2');
+  var pinType = Blockly.Arduino.PinTypes.STEPPER;
+  var stepperName = block.getFieldValue('STEPPER_NAME');
+  var stepperSteps = Blockly.Arduino.valueToCode(block, 'STEPPER_STEPS',
+      Blockly.Arduino.ORDER_ATOMIC) || '360';
+  var stepperSpeed = Blockly.Arduino.valueToCode(block, 'STEPPER_SPEED',
+      Blockly.Arduino.ORDER_ATOMIC) || '90';
+  
+  //stepper is a variable containing the used pins
+  Blockly.Arduino.addVariable(stepperName,
+      'int ' + stepperName + '[2] = {' + pin1 + ', ' + pin2 + '};', true);
+  stepperName = 'stepper_' + stepperName
+
+  Blockly.Arduino.reservePin(block, pin1, pinType, 'Stepper');
+  Blockly.Arduino.reservePin(block, pin2, pinType, 'Stepper');
+
+  Blockly.Arduino.addInclude('stepper', '#include <Stepper.h>');
+
+  var globalCode = 'Stepper ' + stepperName + '(' + stepperSteps + ', ' +
+      pin1 + ', ' + pin2 + ');';
+  Blockly.Arduino.addDeclaration(stepperName, globalCode);
+
+  var setupCode = stepperName + '.setSpeed(' + stepperSpeed + ');';
+  Blockly.Arduino.addSetup(stepperName, setupCode, true);
+
+  return '';
+};
+
+
+/**
  * Code generator for moving the stepper instance (X) a number of steps (Y).
  * Library info in the setHelpUrl link.
  * This block requires the stepper_config block to be present.
@@ -65,6 +107,25 @@ Blockly.Arduino['stepper_config'] = function(block) {
  */
 Blockly.Arduino['stepper_step'] = function(block) {
   var stepperInstanceName = 'stepper_' + block.getFieldValue('STEPPER_NAME');
+  var stepperSteps = Blockly.Arduino.valueToCode(block, 'STEPPER_STEPS',
+      Blockly.Arduino.ORDER_ATOMIC) || '0';
+  var code = stepperInstanceName + '.step(' + stepperSteps + ');\n';
+  return code;
+};
+
+
+/**
+ * Code generator for moving the stepper instance (X) a number of steps (Y).
+ * Library info in the setHelpUrl link.
+ * This block requires the stepper_config block to be present.
+ * Arduino code: loop { X.steps(Y) }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code with order of operation.
+ */
+// // Blockly.Arduino['stepper_step_jwc'] = function(block) {
+Blockly.Arduino['lcd_write_jwc'] = function(block) {
+  // // var stepperInstanceName = 'stepper_' + block.getFieldValue('STEPPER_NAME');
+  var stepperInstanceName = 'lcd_' + block.getFieldValue('STEPPER_NAME');
   var stepperSteps = Blockly.Arduino.valueToCode(block, 'STEPPER_STEPS',
       Blockly.Arduino.ORDER_ATOMIC) || '0';
   var code = stepperInstanceName + '.step(' + stepperSteps + ');\n';
