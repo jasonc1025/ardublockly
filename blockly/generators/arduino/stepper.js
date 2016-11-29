1c3d96d31c7704968301aaf795654ccb917fcfad
jasonc1025 <jasonc@eande.world>
2016-11-29 15:06:25 -0800

    Revert "Feature: Lcd-01b-RealUntestedLcd__In-LcdConfig_Jwc__In-Stepper.js"
    This reverts commit fd06fc537192e1a8ee57c86641d0932ea26cdd19.

--- blockly/generators/arduino/stepper.js
+++ blockly/generators/arduino/stepper.js
@@ -67,71 +67,33 @@ Blockly.Arduino['stepper_config'] = function(block) {
  */
 // // Blockly.Arduino['stepper_config_jwc'] = function(block) {
 Blockly.Arduino['lcd_config_jwc'] = function(block) {
-  // // var pin1 = block.getFieldValue('STEPPER_PIN1');
-  // // var pin2 = block.getFieldValue('STEPPER_PIN2');
-  // // var pinType = Blockly.Arduino.PinTypes.STEPPER;
-  // // var stepperName = block.getFieldValue('STEPPER_NAME');
-  // // var stepperSteps = Blockly.Arduino.valueToCode(block, 'STEPPER_STEPS',
-      // // Blockly.Arduino.ORDER_ATOMIC) || '360';
-  // // var stepperSpeed = Blockly.Arduino.valueToCode(block, 'STEPPER_SPEED',
-      // // Blockly.Arduino.ORDER_ATOMIC) || '90';
+  var pin1 = block.getFieldValue('STEPPER_PIN1');
+  var pin2 = block.getFieldValue('STEPPER_PIN2');
+  var pinType = Blockly.Arduino.PinTypes.STEPPER;
+  var stepperName = block.getFieldValue('STEPPER_NAME');
+  var stepperSteps = Blockly.Arduino.valueToCode(block, 'STEPPER_STEPS',
+      Blockly.Arduino.ORDER_ATOMIC) || '360';
+  var stepperSpeed = Blockly.Arduino.valueToCode(block, 'STEPPER_SPEED',
+      Blockly.Arduino.ORDER_ATOMIC) || '90';
   
-  // // //stepper is a variable containing the used pins
-  // // Blockly.Arduino.addVariable(stepperName,
-      // // 'int ' + stepperName + '[2] = {' + pin1 + ', ' + pin2 + '};', true);
-  // // stepperName = 'stepper_' + stepperName
+  //stepper is a variable containing the used pins
+  Blockly.Arduino.addVariable(stepperName,
+      'int ' + stepperName + '[2] = {' + pin1 + ', ' + pin2 + '};', true);
+  stepperName = 'stepper_' + stepperName
 
-  // // Blockly.Arduino.reservePin(block, pin1, pinType, 'Stepper');
-  // // Blockly.Arduino.reservePin(block, pin2, pinType, 'Stepper');
+  Blockly.Arduino.reservePin(block, pin1, pinType, 'Stepper');
+  Blockly.Arduino.reservePin(block, pin2, pinType, 'Stepper');
 
-  // // Blockly.Arduino.addInclude('stepper', '#include <Stepper.h>');
+  Blockly.Arduino.addInclude('stepper', '#include <Stepper.h>');
 
-  // // var globalCode = 'Stepper ' + stepperName + '(' + stepperSteps + ', ' +
-      // // pin1 + ', ' + pin2 + ');';
-  // // Blockly.Arduino.addDeclaration(stepperName, globalCode);
+  var globalCode = 'Stepper ' + stepperName + '(' + stepperSteps + ', ' +
+      pin1 + ', ' + pin2 + ');';
+  Blockly.Arduino.addDeclaration(stepperName, globalCode);
 
-  // // var setupCode = stepperName + '.setSpeed(' + stepperSpeed + ');';
-  // // Blockly.Arduino.addSetup(stepperName, setupCode, true);
+  var setupCode = stepperName + '.setSpeed(' + stepperSpeed + ');';
+  Blockly.Arduino.addSetup(stepperName, setupCode, true);
 
-  // // return '';
-  
-  
-  // Convert from number to text
-  var i2cControllerDecAddress = Blockly.Arduino.valueToCode(
-      block, 'I2C_CONTROLLER_DEC_ADDRESS_FIELD_ID', Blockly.Arduino.ORDER_ATOMIC) || '39';
-
-
-  var columnsMax = block.getFieldValue('COLUMNS_MAX_FIELD_ID');
-  var rowsMax = block.getFieldValue('ROWS_MAX_FIELD_ID');
-    
-  // * Include is universal for any application of LCD. 
-  // *
-  Blockly.Arduino.addInclude('lcd_TAG_00', '// * For:: ' + Blockly.Msg.ARD_LCD_SETUP_BLOCK_NAME + ': 2016-1119-1700');
-  Blockly.Arduino.addInclude('lcd_TAG_01', '#include <Wire.h>');
-  Blockly.Arduino.addInclude('lcd_TAG_02', '#include <LiquidCrystal_I2C.h>');
-
-  Blockly.Arduino.addDeclaration('lcd_TAG_00', '// * For:: ' + Blockly.Msg.ARD_LCD_SETUP_BLOCK_NAME);
-  Blockly.Arduino.addDeclaration('lcd_TAG_01', 'LiquidCrystal_I2C lcd(' + i2cControllerDecAddress + ',' + columnsMax + ',' + rowsMax + ');');
-  Blockly.Arduino.addDeclaration('lcd_TAG_02', 'String    lcd_OneRow_StringObject;');
-  Blockly.Arduino.addDeclaration('lcd_TAG_03', 'const int lcd_OneRow_Columns_MAX = ' + columnsMax + ';');
-
-  // Allow overwrite by setting last arguement as 'true'
-  Blockly.Arduino.addSetup('lcd_TAG_00', '// * For:: ' + Blockly.Msg.ARD_LCD_SETUP_BLOCK_NAME, true);
-  Blockly.Arduino.addSetup('lcd_TAG_01', 'lcd.init();', true);
-  Blockly.Arduino.addSetup('lcd_TAG_02', 'lcd.backlight();', true);
-  Blockly.Arduino.addSetup('lcd_TAG_03', 'lcd.clear();', true);
-  Blockly.Arduino.addSetup('lcd_TAG_04', 'lcd.setCursor(0,0);', true);
-  Blockly.Arduino.addSetup('lcd_TAG_05', 'lcd.print("TACHnology Bot");', true);
-
-  // Add the code
-  var code = [];
-  code.push('lcd.setCursor(1,0);');
-  code.push('lcd.print("Robotics");');
-
-  //// orig: return code.join('\n') + '\n';
-  return code.join('\n');
-
-  
+  return '';
 };
 
 
