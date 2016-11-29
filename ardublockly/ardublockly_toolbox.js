27ada0df2b9f9763ca6cc46346c847845c2dec5a
jasonc1025 <jasonc@eande.world>
2016-11-28 21:07:27 -0800

    Test Stepper Part 2.2.1a

--- ardublockly/ardublockly_toolbox.js
+++ ardublockly/ardublockly_toolbox.js
@@ -242,13 +242,13 @@ Ardublockly.TOOLBOX_XML =
 '      </value>' +
 '    </block>' +
 /// jwc <<
-'    <block type="stepper_step">' +
-'      <value name="STEPPER_STEPS">' +
-'        <block type="math_number">' +
-'          <field name="NUM">10</field>' +
-'        </block>' +
-'      </value>' +
-'    </block>' +
+// // '    <block type="stepper_step">' +
+// // '      <value name="STEPPER_STEPS">' +
+// // '        <block type="math_number">' +
+// // '          <field name="NUM">10</field>' +
+// // '        </block>' +
+// // '      </value>' +
+// // '    </block>' +
 '  </category>' +
 '  <sep></sep>' +
 '  <category id="catComms" name="Comms">' +
