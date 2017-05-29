bc203f99ac4717dc334716a833e2103b9355cb43
jasonc1025 <jasonc@eande.world>
2017-05-28 19:51:27 -0700

    RUN_ME.bat for convenient startup & Comment for index.php

---
+++ RUN_ME.bat
@@ -0,0 +1,3 @@
+rem This is a convenient RUN_ME file - at root level - to start the index.html for Chrome Browser
+
+start chrome ./ardublockly/index.html
--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header( 'Location: /ardublockly/index.html' ) ;  ?>
+<?php 
+	// 2017-0528-1940
+	// Needed for Heroku framework, to fool it since it runs in PHP
+	// But it not seems to work fully.  It does load index.html but block are corrupted like in the past
+	header( 'Location: /ardublockly/index.html' ) ;  
+?>
